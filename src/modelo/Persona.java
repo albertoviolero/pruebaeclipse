83b4e12902170ca5c8178b18244d1d074a35de60
albertoviolero <albertoviolero@hotmail.com>
2017-11-22 10:28:40 +0100

    Prueba primer commit
    he tocado la clase persona para comprobar que detecta los cambios

--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -15,21 +15,7 @@ public class Persona {
 		super();
 	}
 
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
+	
 	
 	
 	
